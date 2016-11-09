9ea3b534704eb595bfae36a3417cf68d24e8d227
hungln <hungln@ai-t.vn>
2016-11-10 02:15:00 +0700

    Them phan game

--- public/template/ethersmine/js/custom.js
+++ public/template/ethersmine/js/custom.js
@@ -1,22 +1,38 @@
-$(document).ready(function() {
-  "use strict";
-  var $target;
-  //===============Mobile Nav Function============
-    $('#menu').on('click', function() { 
-            $('.navigation').slideToggle();
-    })  
-  });
-	$('a[data-rel^="#"]').on('click.smoothscroll',function (e) {	
-    e.preventDefault();    
+$(document).ready(function () {
+    "use strict";
+    var $target;
+    //===============Mobile Nav Function============
+    $('#menu').on('click', function () {
+        $('.navigation').slideToggle();
+    });
+
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+        }
+    });
+});
+$('a[data-rel^="#"]').on('click.smoothscroll', function (e) {
+    e.preventDefault();
     var target = $(this).attr("data-rel");
-        $target = $(target);
-  goto=parseInt($target.offset().top)-parseInt(62)
+    $target = $(target);
+    goto = parseInt($target.offset().top) - parseInt(62)
     $('html, body').stop().animate({
         'scrollTop': goto
     }, 1000, 'swing', function () {
-       
+
     });
+
+
 });
-$(window).load(function() {
-	$('#loader-wrapper').delay(500).fadeOut();
+
+$(document).ready(function () {
+    //Get the canvas &
+
+    //Initial call
+    //  respondCanvas();
+
+});
+$(window).load(function () {
+    $('#loader-wrapper').delay(500).fadeOut();
 });
